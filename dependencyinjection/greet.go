dcc2b8c819a2e3c9a5d19f796ac9a9a39c8e36cb
PirateKaiju <santosgonsalvesjr@gmail.com>
2020-11-01 09:20:21 -0300

    Dependency Injection

---
+++ dependencyinjection/greet.go
@@ -0,0 +1,22 @@
+package dependencyinjection
+
+//package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	//Greet(os.Stdout, "Test Again")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreetHandler))
+}
