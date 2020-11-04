d0bd905dddf50ede19a00cd451dfc2106d7cd5d8
PirateKaiju <santosgonsalvesjr@gmail.com>
2020-11-04 10:28:36 -0300

    Select

---
+++ select/racer.go
@@ -0,0 +1,28 @@
+package selecttest
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
+	case <-time.After(10 * time.Second):
+		return "timeout!"
+	}
+
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
